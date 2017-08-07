e84b8e27aa01242315d21c60d1f798a3f215c05f
huzongtao <huzongtao@rtmap.com>
2017-08-07 15:15:01 +0800

    JFinal学习demo

---
+++ jf-tomcat/src/com/demo/DemoConfig.java
@@ -0,0 +1,44 @@
+/*
+ * RT MAP, Home of Professional MAP
+ * Copyright  Bit Main Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ */
+package com.demo;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+
+/**
+ * @author huzongtao@rtmap.com
+ * @package com.demo
+ * @date 07/08/2017
+ */
+public class DemoConfig extends JFinalConfig {
+    public void configConstant(Constants me) {
+        me.setDevMode(true);
+    }
+
+    public void configRoute(Routes me) {
+        me.add("/hello",HelloController.class);
+        me.add("index",Indexcontroller.class);
+
+    }
+
+    public void configPlugin(Plugins me) {
+
+    }
+
+    public void configInterceptor(Interceptors me) {
+
+    }
+
+    public void configHandler(Handlers me) {
+
+    }
+}
---
+++ jf-tomcat/src/com/demo/HelloController.java
@@ -0,0 +1,22 @@
+/*
+ * RT MAP, Home of Professional MAP
+ * Copyright 2017 Bit Main Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ */
+package com.demo;
+
+import com.jfinal.core.Controller;
+
+/**
+ * @author huzongtao@rtmap.com
+ * @package com.demo
+ * @date 07/08/2017
+ */
+public class HelloController extends Controller {
+
+    public void index(){
+        renderText("欢迎来到JFinal世界！");
+    }
+}
